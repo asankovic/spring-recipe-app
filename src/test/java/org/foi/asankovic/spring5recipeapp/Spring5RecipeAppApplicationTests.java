f948de98155cc414d9bb806045ad754d103e1525
asankovic <asankovic@foi.hr>
2021-07-27 11:39:18 +0200

    Current project version added to github

---
+++ src/test/java/org/foi/asankovic/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.foi.asankovic.spring5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
