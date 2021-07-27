066f5389899f9de1204aff9238052665223762e5
asankovic <asankovic@foi.hr>
2021-07-27 12:45:23 +0200

    Fixed and checked data model via h2 database

---
+++ src/main/java/org/foi/asankovic/spring5recipeapp/domain/Category.java
@@ -0,0 +1,47 @@
+package org.foi.asankovic.spring5recipeapp.domain;
+
+import org.yaml.snakeyaml.events.Event;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+/**
+ * Created by Sankovic on 27.7.2021.
+ */
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
