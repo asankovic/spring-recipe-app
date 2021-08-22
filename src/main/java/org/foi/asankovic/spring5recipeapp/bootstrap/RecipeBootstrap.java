36cdcf2c83e222571697ab0153fd1281a3ddffb6
asankovic <asankovic@foi.hr>
2021-08-22 13:40:09 +0200

    Trying out switching mysql and h2 database using profiles

--- src/main/java/org/foi/asankovic/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/org/foi/asankovic/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -6,6 +6,7 @@ import org.foi.asankovic.spring5recipeapp.repositories.CategoryRepository;
 import org.foi.asankovic.spring5recipeapp.repositories.RecipeRepository;
 import org.foi.asankovic.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +21,7 @@ import java.util.Optional;
  */
 @Slf4j
 @Component
+@Profile("default")
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private final CategoryRepository categoryRepository;
